d2fd12cacf7f0e2afa22b21d5e2e6dedc1f66730
Girinath V <girinath439@gmail.com>
2026-04-04 14:57:03 +0530

    Create  __init__.py

---
+++ trace_score/__init__.py
@@ -0,0 +1,9 @@
+from .trace import compute_TRACE, TRACEEvaluator, WEIGHT_PRESETS
+from .components.temporal   import compute_T
+from .components.reliability import compute_R
+from .components.adaptive   import compute_A
+from .components.coherence  import compute_C
+from .components.epistemic  import compute_E
+
+__version__ = "0.1.0"
+__author__  = "Girinath V"
