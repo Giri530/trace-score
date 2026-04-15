64d3f6b13de58a6ba65b3b39eb9bd3812d40d25f
Girinath V <girinath439@gmail.com>
2026-04-15 09:27:54 +0530

    Bump version from 0.1.0 to 0.1.1

--- trace_score/__init__.py
+++ trace_score/__init__.py
@@ -5,5 +5,5 @@ from .components.adaptive   import compute_A
 from .components.coherence  import compute_C
 from .components.epistemic  import compute_E
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__  = "Girinath V"
